e32e30959b2f2b506180713f6d0567fe841e02c1
Claudia <claudiaareneee@gmail.com>
2019-12-08 12:30:56 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+/* jshint esversion: 6 */
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+let hostname = '127.0.0.1';
+let port = 3000;
+
+app.use(express.static(__dirname));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    
+    socket.on('something', () => { 
+        console.log('something');
+        let data = "hello world";
+        socket.emit('somethingElse', data);
+    });
+});
+
+http.listen(process.env.PORT || 3000, hostname, function(){
+    console.log('Server running at http://${hostname}:${port}/', hostname, port);
+});
